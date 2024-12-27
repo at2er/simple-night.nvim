61d2f85f494469977c1024fd299a8693a90460a3
at2er <xb0515@outlook.com>
2024-12-27 17:27:39 +0800

    change: palette and highlight group are more comfortable.
    Change:
        color palette be more retro
        most of highlight groups
    
    *not origin styles*

--- lua/simple-night/groups.lua
+++ lua/simple-night/groups.lua
@@ -4,96 +4,94 @@ local utils = require("simple-night.utils")
 local M = {}
 
 local function nvim_groups()
-    return {
-        Normal = {
-            bg = config.transparent and "NONE" or palette.tcolors.black,
-            fg = palette.tcolors.bright_white
-        },
-        NormalFloat = {
-            bg = config.transparent and "NONE" or palette.tcolors.bright_black,
-            fg = palette.tcolors.bright_white
-        },
-        FloatBorder = { fg = palette.tcolors.bright_black },
-        NormalNC = { link = "Normal" },
-        SignColumn = { bg = palette.tcolors.black },
-        ColorColumn = { bg = palette.tcolors.bright_black },
-        Whitespace = { fg = palette.tcolors.white },
-        CursorColumn = { bg = palette.tcolors.bright_black },
-        CursorLine = { bg = palette.tcolors.bright_black },
-        CursorLineNr = { bg = palette.tcolors.black, fg = palette.tcolors.blue },
-        LineNr = { fg = palette.tcolors.bright_black },
-        TermCursor = { bg = palette.tcolors.bright_white, fg = palette.tcolors.bright_black },
-        TermCursorNC = { fg = palette.tcolors.bright_white },
-        WinSeparator = { link = "FloatBorder" },
-        Visual = { bg = palette.tcolors.bright_black },
-        VisualNOS = { bg = palette.tcolors.bright_black },
-        Pmenu = { bg = palette.tcolors.bright_black },
-        PmenuSel = { bg = palette.tcolors.blue, fg = palette.tcolors.black },
-        PmenuKind = { bg = palette.tcolors.bright_black },
-        PmenuKindSel = { bg = palette.tcolors.blue, fg = palette.tcolors.black },
-        PmenuExtra = { bg = palette.tcolors.bright_black },
-        PmenuExtraSel = { bg = palette.tcolors.blue, fg = palette.tcolors.black },
-        PmenuSbar = { bg = palette.tcolors.bright_black },
-        PmenuThumb = { bg = palette.tcolors.bright_white },
-        Search = { bg = palette.tcolors.bright_black },
-        IncSearch = { bg = palette.tcolors.cyan },
-        CurSearch = { link = "IncSearch" },
-        StatusLine = { bg = palette.tcolors.bright_black },
-        StatusLineNC = { bg = palette.tcolors.black },
-        Question = { fg = palette.tcolors.blue },
-        MsgArea = { fg = palette.tcolors.bright_white },
-        WarningMsg = { fg = palette.tcolors.yellow },
-        ErrorMsg = { fg = palette.tcolors.red },
-        MoreMsg = { fg = palette.tcolors.bright_blue },
-        Error = { bg = palette.tcolors.red, fg = palette.tcolors.bright_white },
-        Todo = { bg = palette.tcolors.bright_blue, fg = palette.tcolors.bright_white },
-        Debug = { bg = palette.tcolors.yellow, fg = palette.tcolors.bright_white },
-        Identifier = { fg = palette.tcolors.blue },
-        Function = { fg = palette.tcolors.bright_blue },
-        Statement = { fg = palette.tcolors.bright_red, italic = config.styles.keywords.italic },
-        Conditional = { fg = palette.tcolors.bright_red, italic = config.styles.keywords.italic },
-        Repeat = { fg = palette.tcolors.bright_red, italic = config.styles.keywords.italic },
-        Label = { fg = palette.tcolors.bright_red, italic = config.styles.keywords.italic },
-        Exception = { fg = palette.tcolors.bright_red, italic = config.styles.keywords.italic },
-        Keyword = { fg = palette.tcolors.bright_red, italic = config.styles.keywords.italic },
-        Comment = { fg = palette.tcolors.white, italic = config.styles.comments.italic },
+  return {
+    Normal = {
+      bg = config.transparent and "NONE" or palette.tcolors.black,
+      fg = palette.tcolors.bright_white
+    },
+    NormalFloat = {
+      bg = config.transparent and "NONE" or palette.tcolors.bright_black,
+      fg = palette.tcolors.bright_white
+    },
+    NormalNC      = { link = "Normal" },
+    FloatBorder   = { fg = palette.tcolors.bright_black },
+    SignColumn    = { bg = palette.tcolors.black },
+    ColorColumn   = { bg = palette.colors.bright_bg },
+    CursorColumn  = { bg = palette.colors.bright_bg },
+    CursorLine    = { bg = palette.colors.bright_bg },
+    CursorLineNr  = { bg = palette.tcolors.black, fg = palette.tcolors.bright_yellow },
+    LineNr        = { fg = palette.tcolors.bright_black },
+    TermCursor    = { bg = palette.tcolors.bright_white, fg = palette.tcolors.bright_black },
+    TermCursorNC  = { fg = palette.tcolors.bright_white },
+    Visual        = { bg = palette.tcolors.white, fg = palette.tcolors.black },
+    VisualNOS     = { link = "Visual" },
+    Whitespace    = { fg = palette.tcolors.bright_black },
+    WinSeparator  = { link = "FloatBorder" },
+    Pmenu         = { bg = palette.colors.bright_bg },
+    PmenuSel      = { bg = palette.tcolors.blue, fg = palette.tcolors.black },
+    PmenuKind     = { link = "Pmenu" },
+    PmenuKindSel  = { link = "PmenuSel" },
+    PmenuExtra    = { link = "Pmenu" },
+    PmenuExtraSel = { link = "PmenuSel" },
+    PmenuSbar     = { link = "Pmenu" },
+    PmenuThumb    = { bg = palette.tcolors.white },
+    Search        = { fg = palette.tcolors.black, bg = palette.tcolors.bright_yellow },
+    IncSearch     = { fg = palette.tcolors.black, bg = palette.tcolors.white },
+    CurSearch     = { link = "IncSearch" },
+    StatusLine    = { bg = palette.colors.bright_bg },
+    StatusLineNC  = { bg = palette.tcolors.black },
+    Question      = { fg = palette.tcolors.bright_blue },
+    MsgArea       = { fg = palette.tcolors.bright_white },
+    WarningMsg    = { fg = palette.tcolors.yellow },
+    ErrorMsg      = { fg = palette.tcolors.red },
+    MoreMsg       = { fg = palette.tcolors.bright_blue },
+    Error         = { bg = palette.tcolors.red, fg = palette.tcolors.bright_white },
+    Todo          = { bg = palette.tcolors.bright_blue, fg = palette.tcolors.bright_white },
+    Debug         = { bg = palette.tcolors.yellow, fg = palette.tcolors.bright_white },
 
-        Constant = { fg = palette.tcolors.yellow },
-        String = { fg = palette.tcolors.bright_green },
-        Character = { fg = palette.tcolors.green },
-        Number = { fg = palette.tcolors.bright_green },
-        Boolean = { fg = palette.tcolors.magenta },
-        Operator = { fg = palette.tcolors.bright_magenta },
-        Include = { fg = palette.tcolors.bright_red, italic = true },
-        Define = { fg = palette.tcolors.bright_magenta },
-        PreCondit = { fg = palette.tcolors.bright_magenta, italic = true },
-        Macro = { fg = palette.tcolors.bright_magenta, italic = true },
-        PreProc = { fg = palette.tcolors.bright_magenta, italic = true },
-        Type = { fg = palette.tcolors.bright_red },
-        Special = { fg = palette.tcolors.cyan },
-        SpecialChar = { fg = palette.tcolors.cyan },
-        Tag = { fg = palette.tcolors.bright_cyan },
-        Delimiter = { fg = palette.tcolors.cyan },
-        SpecialComment = { fg = palette.tcolors.white },
+    -- syntax
+    Comment     = { fg = palette.tcolors.blue },
+    Number      = { fg = palette.tcolors.bright_blue },
+    Constant    = { link = "Special" },
+    String      = { fg = palette.tcolors.bright_green },
+    Character   = { link = "String" },
+    Identifier  = { fg = palette.tcolors.bright_white },
+    Function    = { fg = palette.tcolors.white },
+    Statement   = { link = "Special" },
+    PreProc     = { link = "Special" },
+    Type        = { fg = palette.tcolors.yellow },
+    Special     = { fg = palette.tcolors.yellow },
+    SpecialChar = { link = "Character" },
+    Operator    = { fg = palette.tcolors.bright_white },
+    Delimiter   = { fg = palette.tcolors.white },
 
-        Directory = { fg = palette.tcolors.bright_green },
-        Added = { fg = palette.tcolors.bright_green },
-        Changed = { fg = palette.tcolors.bright_yellow },
-        Removed = { fg = palette.tcolors.bright_red },
-        DiffAdd = { bg = palette.tcolors.bright_green },
-        DiffChange = { bg = palette.tcolors.bright_yellow },
-        DiffDelete = { fg = palette.tcolors.bright_red },
-    }
+    Directory  = { fg = palette.tcolors.bright_green },
+    Added      = { fg = palette.tcolors.bright_green },
+    Changed    = { fg = palette.tcolors.bright_yellow },
+    Removed    = { fg = palette.tcolors.bright_red },
+    DiffAdd    = { bg = palette.tcolors.bright_green },
+    DiffChange = { bg = palette.tcolors.bright_yellow },
+    DiffDelete = { fg = palette.tcolors.bright_red },
+
+    MatchParen = { bg = palette.tcolors.cyan },
+
+    markdownH1 = { fg = palette.tcolors.bright_red },
+    markdownH2 = { fg = palette.tcolors.bright_yellow },
+    markdownH3 = { fg = palette.tcolors.bright_green },
+    markdownH4 = { fg = palette.tcolors.bright_magenta },
+    markdownH5 = { fg = palette.tcolors.bright_cyan },
+    markdownH6 = { fg = palette.tcolors.bright_blue },
+  }
 end
 
 function M.init(opts)
-    config = opts
+  config = opts
 
-    utils.set_hl_groups(nvim_groups())
+  utils.set_hl_groups(nvim_groups())
 
-    for _, name in ipairs(config.plugins) do
-        require("simple-night.plugins." .. name).setup(config)
-    end
+  for _, name in ipairs(config.plugins) do
+    require("simple-night.plugins." .. name).setup(config)
+  end
 end
 
 return M
--- lua/simple-night/palette.lua
+++ lua/simple-night/palette.lua
@@ -2,54 +2,28 @@ local M = {}
 
 -- terminal colors
 M.tcolors = {
-    black          = "#14161b",
-    red            = "#A92222",
-    green          = "#3BA92B",
-    yellow         = "#C29D3E",
-    blue           = "#6FA3F8",
-    magenta        = "#B838AC",
-    cyan           = "#3990BF",
-    white          = "#7278A0",
+  black          = "#000000",
+  red            = "#aa0024",
+  green          = "#00aa00",
+  yellow         = "#8c4028",
+  blue           = "#4444aa",
+  magenta        = "#aa00aa",
+  cyan           = "#0066aa",
+  white          = "#666666",
 
-    bright_black   = "#252B35",
-    --bright_black   = "#62676D",
-    bright_red     = "#ED4646",
-    bright_green   = "#63F063",
-    bright_yellow  = "#EFBF58",
-    bright_blue    = "#3E71C2",
-    bright_magenta = "#D266C7",
-    bright_cyan    = "#52CEE7",
-    bright_white   = "#C6CCF4",
+  bright_black   = "#2c2c2c",
+  bright_red     = "#cc1000",
+  bright_green   = "#48bb48",
+  bright_yellow  = "#bbbb55",
+  bright_blue    = "#5555ff",
+  bright_magenta = "#bc33cc",
+  bright_cyan    = "#0088ff",
+  bright_white   = "#999999",
 }
 
----@deprecated
 M.colors = {
-    -- dark colors
-    dark0 = "#090b0f",
-    dark1 = "#14161b",
-    dark2 = "#1a1d23",
-    gray0 = "#07080d",
-    blue0 = "#285992",
-    blue1 = "#5871e2",
-    blue2 = "#6981F0",
-    red0 = "#b12729",
-
-    -- light colors
-    light0 = "#e0e2ea",
-    light1 = "#a5a8af",
-    light_gray0 = "#c4c6cc",
-    light_gray1 = "#5f6168",
-    light_gray2 = "#45484f",
-    light_yellow0 = "#fce094",
-    light_red0 = "#e74343",
-    light_green0 = "#98eb82",
-    light_green1 = "#64b842",
-    light_blue0 = "#7baee9",
-    light_blue1 = "#3f79bb",
-    light_blue2 = "#215a9b",
-    light_orange0 = "#cb871f",
-    light_purple0 = "#c182d3",
-    light_pink0 = "#ea8bef",
+  bg        = M.tcolors.black,
+  bright_bg = "#101010",
 }
 
 return M
